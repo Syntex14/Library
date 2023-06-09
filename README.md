1e8e4fd0e8bc08b32cc4909cc7d0988f0c84ea52
Syntex14 <rangelabraham14@gmail.com>
2023-06-08 23:05:42 -0500

    Need to complete event clicker for submit button and input forms

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Library
+
+Hi!
+
+I am going to create a basic library application. The main of this project is gather experience in using objects, object constructors and prototypal inheritance. 
+
+
---
+++ script.js
@@ -0,0 +1,17 @@
+let myLibrary = [];
+
+function Book(book, author, bookStatus) {
+    // will be used as a constructor
+    this.book = book;
+    this.author = author;
+    this.bookStatus = bookStatus;
+
+}
+
+function addBookToLibrary () {
+
+}
+
+// function submitBook() 
+    // const bookInputSelector = document.querySelector(".book-input")
+        // 
