3c3470ea6ef2ad76c7713f17418f4b52e3a8e8d3
Syntex14 <rangelabraham14@gmail.com>
2023-06-12 12:22:56 -0500

    Completed signficant portions of project

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let myLibrary = [];
 
+
 function Book(book, author, bookStatus) {
     // will be used as a constructor
     this.book = book;
@@ -8,10 +9,32 @@ function Book(book, author, bookStatus) {
 
 }
 
-function addBookToLibrary () {
-
+function addBookToLibrary (bookPropertiesArray) {
+    myLibrary.push(new Book(...bookPropertiesArray));
 }
 
-// function submitBook() 
-    // const bookInputSelector = document.querySelector(".book-input")
-        // 
+const bookRegistration = document.querySelector("#book-form");
+bookRegistration.addEventListener("submit", getBook);
+
+
+function getBook (e) {
+    e.preventDefault();
+    
+    const bookName = Array.from(document.querySelectorAll("#book-form input"));
+    const bookPropertyArray = [];
+    bookName.forEach((bookProperty) => {
+        bookPropertyArray.push(bookProperty.value);
+        
+    })
+    
+    addBookToLibrary(bookPropertyArray);
+    buildLibraryTableRow();
+}
+
+function buildLibraryTableRow () {
+    let tableRow = "<tr><td></td><td></td><td></td></tr>";
+    const getTableRow = document.getElementById("t-body")
+    getTableRow.insertAdjacentHTML("beforeend", tableRow);
+    console.log(getTableRow);
+    
+}
